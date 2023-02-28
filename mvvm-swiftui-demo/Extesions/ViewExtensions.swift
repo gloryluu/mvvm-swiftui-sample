4fc9294606a922e0c7f066d82138210b7509d342
Quang V. Luu <vinhquang.luu@officience.com>
2023-02-28 11:47:33 +0700

    Fix to comply swiftui

--- mvvm-swiftui-demo/Extesions/ViewExtensions.swift
+++ mvvm-swiftui-demo/Extesions/ViewExtensions.swift
@@ -9,22 +9,23 @@ import Foundation
 import SwiftUI
 
 extension View {
-    func errorAlert(error: Binding<Error?>, buttonTitle: String = "Ok") -> some View {
+    @ViewBuilder func errorAlert(error: Binding<Error?>, buttonTitle: String = "Ok") -> some View {
         let localizedAlertError = LocalizedAlertError(error: error.wrappedValue)
         
-//        if #available(iOS 15.0, *) {
-//            return alert(isPresented: .constant(localizedAlertError != nil), error: localizedAlertError) { _ in
-//                Button(buttonTitle) {
-//                    error.wrappedValue = nil
-//                }
-//            } message: { error in
-//                Text(error.recoverySuggestion ?? "")
-//            }
-//        }
-        return alert(isPresented: .constant(localizedAlertError != nil)) {
-            Alert(
-                title: Text(localizedAlertError?.localizedDescription ?? ""), message: Text(localizedAlertError?.recoverySuggestion ?? ""), dismissButton: .default(Text(buttonTitle))
-            )
+        if #available(iOS 15.0, *) {
+            alert(isPresented: .constant(localizedAlertError != nil), error: localizedAlertError) { _ in
+                Button(buttonTitle) {
+                    error.wrappedValue = nil
+                }
+            } message: { error in
+                Text(error.recoverySuggestion ?? "")
+            }
+        } else {
+            alert(isPresented: .constant(localizedAlertError != nil)) {
+                Alert(
+                    title: Text(localizedAlertError?.localizedDescription ?? ""), message: Text(localizedAlertError?.recoverySuggestion ?? ""), dismissButton: .default(Text(buttonTitle))
+                )
+            }
         }
     }
 }
